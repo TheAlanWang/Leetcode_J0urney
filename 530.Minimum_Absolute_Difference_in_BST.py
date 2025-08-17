f326ffa6b8cee056f7f89d64f1cf3997d4a814f9
Alanxwww <alanwang166@gmail.com>
2025-08-17 10:00:55 -0700

    update 081725

---
+++ 530.Minimum_Absolute_Difference_in_BST.py
@@ -0,0 +1,28 @@
+# https://leetcode.com/problems/minimum-absolute-difference-in-bst/description/
+# O(n) O(h)
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+from typing import Optional
+
+class Solution:
+    def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
+        prev = None
+        ans = float('inf')
+
+        def inorder(node: Optional[TreeNode]) -> None:
+            nonlocal prev, ans
+            if not node:
+                return
+            inorder(node.left)
+            if prev is not None:
+                ans = min(ans, node.val - prev)  # 中序保证非负
+            prev = node.val
+            inorder(node.right)
+
+        inorder(root)
+        return ans
---
+++ 57.Insert_Interval.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/insert-interval/description/
+# O(n) O(1)
+
+from typing import List
+
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        res = []
+        n = len(intervals)
+        i = 0
+        start, end = newInterval
+        while i < n and intervals[i][1] < start:
+            res.append(intervals[i])
+            i += 1
+
+        while i < n and intervals[i][0] <= end:
+            start = min(start, intervals[i][0])
+            end = max(end, intervals[i][1])
+            i += 1
+        res.append([start, end])
+
+        while i < n:
+            res.append(intervals[i])
+            i += 1
+        return res
